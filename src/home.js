c4c1cac2c05fa97c10aeb82777c39563987a5416
simon-wdev <214550778+simon-wdev@users.noreply.github.com>
2026-04-06 08:23:00 +0200

    Add root colors
    Added root colors and basic style for the template header.

--- src/home.js
+++ src/home.js
@@ -1,16 +1,22 @@
 
 //Homepage
-
+import homeJpg from "./homeJpg.jpg";
 
 export default function buildHomepage(){
     const wrapper = document.createElement("div")
-
+    wrapper.classList.add("home-wrapper")
     let header = document.createElement("h1");
+
+    let bodywrapper = document.createElement("div")
+    bodywrapper.classList.add("body-wrapper")
+    let img = document.createElement("img");
     let body = document.createElement("p");
+    bodywrapper.append(img, body)
 
     header.textContent = "The Patty Project";
+    img.src = homeJpg;
     body.textContent = "Vergiss labberige Mensa-Patties. Wir bringen das Grill-Feeling direkt in dein Viertel. Handgemacht, frisch und mit der Extraportion Liebe (und Käse) zubereitet. Dein Hunger hat endlich einen Endgegner gefunden!";
 
-    wrapper.append(header, body);
+    wrapper.append(header, bodywrapper);
     return wrapper
 }
